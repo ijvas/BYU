5592f1da0cc2d8d3ec075a72afd3e87294356ef5
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-04 05:28:46 -0300

    CSE111 - We've created the squeleton of the w1 individual assignment

--- 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
+++ 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
@@ -1,2 +1,11 @@
-import datetime
+from datetime import datetime
 
+today = datetime.now()
+week_day = today.weekday()
+
+subtotal = float(input('Please, enter the subtotal: '))
+
+if(week_day != 1 and week_day != 2):
+    print(f'today is another day')
+else
+    
