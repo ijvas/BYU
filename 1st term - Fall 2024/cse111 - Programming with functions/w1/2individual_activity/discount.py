29b0d4de974170a90bdb3f678d68234ffefb79f7
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-06 10:42:16 -0300

    CSE111 - w1 individual assignment- 2nd stretch challenge CORRECTED

--- 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
+++ 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
@@ -3,26 +3,30 @@ from datetime import datetime
 today = datetime.now()
 week_day = today.weekday()
 
-subtotal = float(input('Please, enter the subtotal: '))
+price = 1
+subtotal = 0
 
-while (subtotal != 0):
-    if( (subtotal >= 50) and (week_day == 1 or week_day == 2) ):
-        discount_ten_percent = subtotal * 0.1
-        subtotal_with_discount = subtotal - discount_ten_percent
-        tax_six_percent = subtotal_with_discount * 0.06
-        total = subtotal_with_discount + tax_six_percent
-        print(f'Discount amount: {discount_ten_percent:.2f}')
-        print(f'Sales tax amount: {tax_six_percent:.2f}')
-        print(f'Total: {total:.2f}')    
-    elif( (subtotal < 50) and (week_day == 1 or week_day == 2) ):
-        amount_needed = 50 - subtotal
-        print(f'The additional amount you need to purchase in order to receive the discount is {amount_needed:.2f}')
-    else:
-        tax_six_percent = subtotal * 0.06
-        total = subtotal + tax_six_percent
-        print(f'Sales tax amount: {tax_six_percent:.2f}')
-        print(f'Total: {total:.2f}')    
-        
-    subtotal = float(input('Please, enter the subtotal: '))
+while (price != 0):
+    price = int(input('Enter the price of the product: '))
+    if(price != 0):
+        quantity = int(input('How much products do you buy for that price: '))
+        subtotal = price * quantity
+        if( (subtotal >= 50) and (week_day == 1 or week_day == 2) ):
+            discount_ten_percent = subtotal * 0.1
+            subtotal_with_discount = subtotal - discount_ten_percent
+            tax_six_percent = subtotal_with_discount * 0.06
+            total = subtotal_with_discount + tax_six_percent
+            print(f'Discount amount: {discount_ten_percent:.2f}')
+            print(f'Sales tax amount: {tax_six_percent:.2f}')
+            print(f'Total: {total:.2f}')    
+        elif( (subtotal < 50) and (week_day == 1 or week_day == 2) ):
+            amount_needed = 50 - subtotal
+            print(f'The additional amount you need to purchase in order to receive the discount is {amount_needed:.2f}')
+        else:
+            tax_six_percent = subtotal * 0.06
+            total = subtotal + tax_six_percent
+            print(f'Sales tax amount: {tax_six_percent:.2f}')
+            print(f'Total: {total:.2f}')    
+
 
     
