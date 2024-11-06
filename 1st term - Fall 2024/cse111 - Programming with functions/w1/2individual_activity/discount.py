ebda7e0401921bd0fd634bd237c576e0d8f38a21
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-06 10:18:22 -0300

    CSE111 - w1 individual assignment- 2nd stretch challenge

--- 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
+++ 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
@@ -5,21 +5,24 @@ week_day = today.weekday()
 
 subtotal = float(input('Please, enter the subtotal: '))
 
-if( (subtotal >= 50) and (week_day == 1 or week_day == 2) ):
-    discount_ten_percent = subtotal * 0.1
-    subtotal_with_discount = subtotal - discount_ten_percent
-    tax_six_percent = subtotal_with_discount * 0.06
-    total = subtotal_with_discount + tax_six_percent
-    print(f'Discount amount: {discount_ten_percent:.2f}')
-    print(f'Sales tax amount: {tax_six_percent:.2f}')
-    print(f'Total: {total:.2f}')    
-elif( (subtotal < 50) and (week_day == 1 or week_day == 2) ):
-    amount_needed = 50 - subtotal
-    print(f'The additional amount you need to purchase in order to receive the discount is {amount_needed:.2f}')
-else:
-    tax_six_percent = subtotal * 0.06
-    total = subtotal + tax_six_percent
-    print(f'Sales tax amount: {tax_six_percent:.2f}')
-    print(f'Total: {total:.2f}')    
+while (subtotal != 0):
+    if( (subtotal >= 50) and (week_day == 1 or week_day == 2) ):
+        discount_ten_percent = subtotal * 0.1
+        subtotal_with_discount = subtotal - discount_ten_percent
+        tax_six_percent = subtotal_with_discount * 0.06
+        total = subtotal_with_discount + tax_six_percent
+        print(f'Discount amount: {discount_ten_percent:.2f}')
+        print(f'Sales tax amount: {tax_six_percent:.2f}')
+        print(f'Total: {total:.2f}')    
+    elif( (subtotal < 50) and (week_day == 1 or week_day == 2) ):
+        amount_needed = 50 - subtotal
+        print(f'The additional amount you need to purchase in order to receive the discount is {amount_needed:.2f}')
+    else:
+        tax_six_percent = subtotal * 0.06
+        total = subtotal + tax_six_percent
+        print(f'Sales tax amount: {tax_six_percent:.2f}')
+        print(f'Total: {total:.2f}')    
+        
+    subtotal = float(input('Please, enter the subtotal: '))
 
     
