48258c57106a6af2b9c27e68856fb1c00beed319
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-06 10:01:17 -0300

    CSE111 - w1 individual assignment- 1st stretch challenge

--- 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
+++ 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
@@ -13,6 +13,9 @@ if( (subtotal >= 50) and (week_day == 1 or week_day == 2) ):
     print(f'Discount amount: {discount_ten_percent:.2f}')
     print(f'Sales tax amount: {tax_six_percent:.2f}')
     print(f'Total: {total:.2f}')    
+elif( (subtotal < 50) and (week_day == 1 or week_day == 2) ):
+    amount_needed = 50 - subtotal
+    print(f'The additional amount you need to purchase in order to receive the discount is {amount_needed:.2f}')
 else:
     tax_six_percent = subtotal * 0.06
     total = subtotal + tax_six_percent
