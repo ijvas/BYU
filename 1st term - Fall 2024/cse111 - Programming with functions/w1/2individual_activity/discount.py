af4e4154c65fe9fea0402d34be5779f850f0bad8
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-06 09:53:14 -0300

    CSE111 - w1 individual assignment-core requirements

--- 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
+++ 1st term - Fall 2024/cse111 - Programming with functions/w1/2individual_activity/discount.py
@@ -5,7 +5,18 @@ week_day = today.weekday()
 
 subtotal = float(input('Please, enter the subtotal: '))
 
-if(week_day != 1 and week_day != 2):
-    print(f'today is another day')
-else
+if( (subtotal >= 50) and (week_day == 1 or week_day == 2) ):
+    discount_ten_percent = subtotal * 0.1
+    subtotal_with_discount = subtotal - discount_ten_percent
+    tax_six_percent = subtotal_with_discount * 0.06
+    total = subtotal_with_discount + tax_six_percent
+    print(f'Discount amount: {discount_ten_percent:.2f}')
+    print(f'Sales tax amount: {tax_six_percent:.2f}')
+    print(f'Total: {total:.2f}')    
+else:
+    tax_six_percent = subtotal * 0.06
+    total = subtotal + tax_six_percent
+    print(f'Sales tax amount: {tax_six_percent:.2f}')
+    print(f'Total: {total:.2f}')    
+
     
