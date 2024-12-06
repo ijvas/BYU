edcf1de7e277492b5ed6ea632b92b716796d4191
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-12-06 19:05:33 -0300

    CSE111 - W5 Prepare-Learning Activities: Text Files - Assignment

--- 1st term - Fall 2024/cse111 - Programming with functions/w5/1_prepare_learning_activities/1_text_files/assignment/provinces.py
+++ 1st term - Fall 2024/cse111 - Programming with functions/w5/1_prepare_learning_activities/1_text_files/assignment/provinces.py
@@ -1,17 +1,35 @@
-import csv 
+import csv
 
-main():
+def main():
+    
+    provinces_list = read_file('provinces.txt')
+
+    provinces_list.pop(0)
+    provinces_list.pop()
+
+    for i in range (len(provinces_list)):
+        if provinces_list[i] == 'AB':
+            provinces_list[i] = 'Alberta'
+
+    counter = provinces_list.count('Alberta')
+
+    print(provinces_list)
+    print(f'Alberta appears {counter} times in the modified list.')
 
 
+def read_file(file_name):
 
+    with open(file_name, 'rt') as file:
 
-def read_file():
+        provinces = []
 
-    with open(provinces.txt, "rt") as file:
-        reader = csv.reader(file)
+        for line in file:
 
-        for row_list in file:
-            
+            clean_line = line.strip()
 
-if __name__ == "__main__":
+            provinces.append(clean_line)
+        
+    return provinces
+
+if __name__ == '__main__':
     main()
